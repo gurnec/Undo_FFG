6315fa4a72eb20d65f96ae01ff2b665a93429cbb
Christopher Gurnee <chris@gurneeconsulting.net>
2017-12-13 10:02:23 -0500

    look for NSIS in registry in build_installer.py

--- installer/build_installer.py
+++ installer/build_installer.py
@@ -1,6 +1,6 @@
 #!python3.6-32
 
-import sys, os, atexit, msvcrt
+import sys, os, winreg
 from subprocess import run, PIPE
 from pathlib import Path
 
@@ -11,15 +11,20 @@ elif len(sys.argv) > 1:
     sys.exit(f'Usage: {Path(sys.argv[0]).name} [--sign CERTIFICATE-NAME]')
 else:
     cert_name = None
-
-atexit.register(lambda: (print('\nPress any key to exit ...', end='', flush=True), msvcrt.getch()))
+    import atexit, msvcrt
+    atexit.register(lambda: (print('\nPress any key to exit ...', end='', flush=True), msvcrt.getch()))
 
 vc_redist = Path(__file__).parent / 'vc_redist.x86.exe'
 assert vc_redist.is_file()
 
-program_files = os.getenv('ProgramFiles(x86)') or os.getenv('ProgramFiles')
-assert program_files
-makensis = Path(program_files) / r'NSIS\makensis.exe'
+try:
+    with winreg.OpenKeyEx(winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\NSIS',
+                          access=winreg.KEY_QUERY_VALUE | winreg.KEY_WOW64_32KEY) as regkey:
+        makensis = Path(winreg.QueryValueEx(regkey, None)[0]) / 'makensis.exe'
+except OSError:
+    program_files = os.getenv('ProgramFiles(x86)') or os.getenv('ProgramFiles')
+    assert program_files
+    makensis = Path(program_files) / r'NSIS\makensis.exe'
 assert makensis.is_file()
 
 output = run(('py', '-3.6-32', '-c', 'import sys; sys.stdout.buffer.write(sys.executable.encode("utf-8"))'),
